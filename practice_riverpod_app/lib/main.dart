79e0fe7549fd3c83fbed1ba180983da884dfe95a
sttagami2 <sttagami2@gmail.com>
2022-08-27 20:34:55 +0900

    カウンターアプリ作成、MVVMにて実装

--- practice_riverpod_app/lib/main.dart
+++ practice_riverpod_app/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:practice_riverpod_app/data/count_data.dart';
 import 'package:practice_riverpod_app/provider.dart';
+import 'package:practice_riverpod_app/view_model.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -17,53 +20,73 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends ConsumerStatefulWidget {
+  MyHomePage({
+    Key? key,
+  }) : super(key: key);
 
-  final String title;
+  @override
+  ConsumerState<MyHomePage> createState() => _MyHomePageState();
+}
 
-  int _counter = 0;
+class _MyHomePageState extends ConsumerState<MyHomePage> {
+  ViewModel _viewModel = ViewModel();
 
-  void _incrementCounter() {}
+  @override
+  void initState() {
+    super.initState();
+
+    _viewModel.setRef(ref);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Consumer(
-              builder: (BuildContext context, WidgetRef ref, Widget? child) =>
-                  Text(ref.watch(titleProvider)))),
+        title: Text(ref.watch(titleProvider)),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Consumer(
-              builder: (context, ref, child) => Text(
-                ref.watch(messageProvider),
-              ),
+            Text(
+              ref.watch(messageProvider),
             ),
-            Consumer(
-              builder: (context, ref, child) => Text(
-                ref.watch(countProvider).toString(),
-                style: Theme.of(context).textTheme.headline4,
-              ),
+            Text(
+              _viewModel.count,
+              style: Theme.of(context).textTheme.headline4,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                  onPressed: () => _viewModel.onIncrease(),
+                  child: const Icon(CupertinoIcons.plus),
+                ),
+                FloatingActionButton(
+                  onPressed: () => _viewModel.onDecrease(),
+                  child: const Icon(CupertinoIcons.minus),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(_viewModel.countUp),
+                Text(_viewModel.countDown),
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: Consumer(
-        builder: (context, ref, child) {
-          return FloatingActionButton(
-            onPressed: () => ref.read(countProvider.state).state++,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          );
-        },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _viewModel.onReset(),
+        child: const Icon(Icons.refresh),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
--- practice_riverpod_app/lib/provider.dart
+++ practice_riverpod_app/lib/provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:practice_riverpod_app/data/count_data.dart';
 
 final titleProvider = Provider<String>((ref) {
   return 'Riverpod Demo Home Page';
@@ -9,3 +10,6 @@ final messageProvider = Provider<String>((ref) {
 });
 
 final countProvider = StateProvider<int>((ref) => 0);
+
+final countDataProvider = StateProvider<CountData>(
+    (ref) => const CountData(count: 0, countUp: 0, countDown: 0));
